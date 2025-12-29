6f9e8c5650fb0dcaed37f08a0e6a594095aa388b
Tisa <tisatandel@gmail.com>
2025-12-29 18:05:42 +0530

    all done

---
+++ src/app/API/employee-service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface Employee {
+  id: number;
+  name: string;
+  email: string;
+  phone: number;
+  gender: string;
+  salary: number;
+  city: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EmployeeService {
+  private http = inject(HttpClient);
+  private baseUrl = "http://localhost:3000/employee";
+
+  // GET 
+  getEmployee(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(this.baseUrl);
+  }
+
+  // POST
+  addEmployee(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(this.baseUrl, employee);
+  }
+
+  // DELETE
+  deleteEmployee(id: number): Observable<Employee> {
+    return this.http.delete<Employee>(`${this.baseUrl}/${id}`);
+  }
+
+  
+}
--- src/app/form/form.ts
+++ src/app/form/form.ts
@@ -1,35 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { EmployeeService, Employee } from '../API/employee-service';
 
-interface EmployeeForm
-{
-  name:FormControl<string | null >;
-  email:FormControl<string | null >;
-  phone:FormControl<number | null >;
-  gender:FormControl<string | null >;
-  salary:FormControl<number | null >;
-  city:FormControl<string | null >;
-
+interface EmployeeForm {
+  id: FormControl<number | null>;
+  name: FormControl<string | null>;
+  email: FormControl<string | null>;
+  phone: FormControl<number | null>;
+  gender: FormControl<string | null>;
+  salary: FormControl<number | null>;
+  city: FormControl<string | null>;
 }
+
 @Component({
   selector: 'app-form',
   imports: [ReactiveFormsModule],
   templateUrl: './form.html',
-  styleUrl: './form.css',
+  styleUrls: ['./form.css'],
 })
 export class Form {
-  form = new FormGroup<EmployeeForm>({
-    name:new FormControl (null,Validators.required),
-    email:new FormControl (null,[Validators.required ,Validators.pattern(/^[a-zA-Z0-9._%+-]+@tag97(\.[a-zA-Z]{2,})?$/)]),
-    phone: new FormControl(null, [Validators.required,Validators.pattern(/^[6-9]\d{9}$/)]),
-    gender:new FormControl (null,Validators.required),
-    salary:new FormControl (null,[Validators.required,Validators.min(10000),Validators.max(50000)]),
-    city:new FormControl (null,Validators.required)
-  })
 
-  OnSubmit(){
+  private http = inject(EmployeeService);
+  
+
+  form = new FormGroup<EmployeeForm>({
+    id: new FormControl(null, Validators.required),
+    name: new FormControl(null, Validators.required),
+    email: new FormControl(null, [
+      Validators.required,
+      Validators.pattern(/^[a-zA-Z0-9._%+-]+@tag97(\.[a-zA-Z]{2,})?$/)
+    ]),
+    phone: new FormControl(null, [
+      Validators.required,
+      Validators.pattern(/^[6-9]\d{9}$/)
+    ]),
+    gender: new FormControl(null, Validators.required),
+    salary: new FormControl(null, [
+      Validators.required,
+      Validators.min(10000),
+      Validators.max(50000)
+    ]),
+    city: new FormControl(null, Validators.required)
+
+  });
+
+  OnSubmit() {
     console.log('Form Submitted');
     console.log(this.form);
     console.log(this.form.value);
+
+    this.http.getEmployee().subscribe((employees) => {
+      console.log(employees);
+    });
+
+    this.http.addEmployee(this.form.value as Employee).subscribe((employee) => {
+      console.log('Employee added:', employee);
+    });
+
+    this.http.deleteEmployee(id).subscribe((employee) => {
+        console.log('Employee deleted:', employee);
+      });
+
   }
 }
